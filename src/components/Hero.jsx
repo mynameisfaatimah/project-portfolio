be71c0526105596bc8f48f3a8f02929e5e497e70
Faatimah Cleveland <faatimahcleveland@gmail.com>
2024-04-09 14:08:16 -0400

    edited z-index and adjusted images positioning and size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,11 +11,11 @@ const Hero = () => {
   return (
     <div className='relative grid sm:grid-cols-3 gap-8 max-w-[1200px] md:h-[70vh] mx-auto py-8 overflow-hidden' id='hero'>
         
-     <img className='hidden md:block absolute z-[-1] w-[500px] bottom-0 left-[-120px]' src={notebookL} alt=""/>  
-     <img className='absolute z-[-1] w-[400px] bottom-[-100px] left-[320px]' src={notebookM} alt=""/>
-     <img className='absolute z-[-1] w-[250px] bottom-[200px] md:bottom-[300px] left-[450px]' src={mobileR} alt=""/>
+     <img className='hidden md:block absolute z-[-1] w-[500px] bottom-[-90] left-[-140px]' src={notebookL} alt=""/>  
+     <img className='absolute z-[-1] w-[400px] bottom-[-40px] left-[210px]' src={notebookM} alt=""/>
+     <img className='absolute z-[-1] w-[250px] bottom-[200px] md:bottom-[280px] left-[390px]' src={mobileR} alt=""/>
 
-     <div className='glass px-5 cols-span-2 my-auto '>
+     <div className='glass px-5 cols-span-2 my-auto z-[-1]'>
         <h1 className= 'text-white text-4xl sm:text-5xl lg:text-7xl font-extrabold'>
             <p className='mb-4 text-gray-500'>I'm a</p>
             <TypeAnimation 
@@ -37,7 +37,7 @@ const Hero = () => {
         </p>
     </div>
 
-<div className='absolute bottom-0 right-0 w-[200px] lg:w-[600px]' >
+<div className='absolute bottom-0 right-0 w-[160px] lg:w-[600px]' >
 <img  src={profilepic} alt=""/>
 </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,11 @@ const Navbar = () => {
     setNav(!nav);
   };
   return (
-    <div className=' glass-nav px-8  text-gray-300 h-[96px] max-w-[1200px] flex justify-between items-center mx-auto'>
+    <div className=' glass-nav px-8 md:backdrop-blur-x1 text-gray-300 h-[96px] max-w-[1200px] flex justify-between items-center mx-auto'>
       <h1 className='text-xl font-bold'>Faatimah</h1>
 
       <ul className='hidden md:flex gap-6 text-xl'>
-        <li> <Link to="home" spy={true} smooth={true} offset={50} duration={500}> Home </Link> </li>
+        <li > <Link to="home" spy={true} smooth={true} offset={50} duration={500}> Home </Link> </li>
         <li> <Link to="about" spy={true} smooth={true} offset={50} duration={500}> About </Link> </li>
         <li> <Link to="work" spy={true} smooth={true} offset={50} duration={500}> Portfolio </Link> </li>
         <li> <Link to="contact" spy={true} smooth={true} offset={50} duration={500}>Contact</Link></li>
@@ -24,7 +24,7 @@ const Navbar = () => {
             </div>
 
       <div className={nav ? 'text-gray-300 z-40 fixed left-0 top-0 w-full bg-[#232323] ease-in-out duration-500 ' : 'fixed left-[-100%]'}>
-        <ul className='p-8 text-4xl ml-20 z-10'>
+        <ul className='p-8 text-4xl ml-20 z-20'>
           <li className="p-2"><Link to="home" spy={true} smooth={true} offset={50} duration={500}>Home</Link></li>
           <li className="p-2"><Link to="about" spy={true} smooth={true} offset={50} duration={500}>About</Link></li>
           <li className="p-2"><Link to="work" spy={true} smooth={true} offset={50} duration={500}>Portfolio</Link></li>
